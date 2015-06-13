361f42eee696bcb8d96dd5921615da1696a4f830
Aaron Williams <theadwilliams@gmail.com>
2015-06-13 15:40:46 -0700

    added Rdio sdk and dependencies

--- SongDecider/AppDelegate.m
+++ SongDecider/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Rdio/Rdio.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    Rdio *rdio = [[Rdio alloc] initWithClientId:@"YOUR CLIENT ID" andSecret:@"YOUR SECRET" delegate:nil];
+    [rdio preparePlayerWithDelegate:nil];
+    [rdio.player performSelector:@selector(play:) withObject:@"t1" afterDelay:2.0];
     return YES;
 }
 
