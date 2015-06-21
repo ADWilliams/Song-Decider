86471929f24ec03fe6cecf1235a42577fb78946e
iantsai <iantsai86@gmail.com>
2015-06-21 12:37:57 -0700

    first commit

---
+++ SongDecider/SlideMenuCategoryCell.h
@@ -0,0 +1,13 @@
+//
+//  SlideMenuCategoryCell.h
+//  SongDecider
+//
+//  Created by Ian Tsai on 2015-06-21.
+//  Copyright (c) 2015 Aaron Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlideMenuCategoryCell : UITableViewCell
+
+@end
---
+++ SongDecider/SlideMenuCategoryCell.m
@@ -0,0 +1,23 @@
+//
+//  SlideMenuCategoryCell.m
+//  SongDecider
+//
+//  Created by Ian Tsai on 2015-06-21.
+//  Copyright (c) 2015 Aaron Williams. All rights reserved.
+//
+
+#import "SlideMenuCategoryCell.h"
+
+@implementation SlideMenuCategoryCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
